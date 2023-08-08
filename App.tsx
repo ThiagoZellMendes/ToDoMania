4cd71fdb7924195579ce435f6ad9d447a4cc5f47
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-08-08 19:00:00 -0300

    Fix: StyledComponentsGlobalTheme

--- App.tsx
+++ App.tsx
@@ -1,18 +1,15 @@
+import React from "react"
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
-import { ThemeProvider } from "styled-components"
-import theme from "./src/global/styles/theme"
+import { Text } from "react-native"
+import { ThemeProvider } from "styled-components/native"
+import  theme  from "./src/global/styles/theme"
 import { Header } from "./src/components/header/Header"
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <>
-        <Header>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </Header>
-      </>
+      <Header />
+      <StatusBar style="auto" />
     </ThemeProvider>
   )
 }
